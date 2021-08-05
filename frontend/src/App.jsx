8ea1b32356b3eb432852369cefbad548f416a469
MohammdJavad14 <mrph14@gmail.com>
2021-08-05 17:04:40 +0430

    Prepare for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScrenn';
 import CartScreen from './screens/CartScreen';
--- frontend/src/screens/SearchScreen.jsx
+++ frontend/src/screens/SearchScreen.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import SearchBox from '../components/SearchBox';
-import { Route } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 import { listProducts } from '../actions/productActions';
 import Card from '../components/UI/Card';
+import Message from '../components/UI/Message';
 import Row from '../components/UI/Row';
 
 import classes from './SearchScreen.module.css';
+import Loader from '../components/UI/Loader';
 
 const SearchScreen = ({ match }) => {
   const productList = useSelector((state) => state.productList);
@@ -21,25 +23,33 @@ const SearchScreen = ({ match }) => {
   return (
     <div className={classes.container}>
       <Route render={({ history }) => <SearchBox history={history} />} />
-      {products.map((product) => (
-        <Card className='ss' key={product.product}>
-          <div className={classes['img-container']}>
-            <img src={product.image} alt={product.name} />
-          </div>
-          <div className={classes['product-details']}>
-            <h5 className={classes['product-name']}>{product.name}</h5>
-            <Row>
-              <h5 className={classes['product-stock']}>
-                {product.countInStock < 1
-                  ? 'ناموجود'
-                  : product.countInStock < 6
-                  ? `فقط ${product.countInStock} عدد باقی مانده `
-                  : 'موجود در انبار'}
-              </h5>
-            </Row>
-          </div>
-        </Card>
-      ))}
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message>{error}</Message>
+      ) : (
+        products.map((product) => (
+          <Link to={`/product/${product._id}`}>
+            <Card className='ss' key={product.product}>
+              <div className={classes['img-container']}>
+                <img src={product.image} alt={product.name} />
+              </div>
+              <div className={classes['product-details']}>
+                <h5 className={classes['product-name']}>{product.name}</h5>
+                <Row>
+                  <h5 className={classes['product-stock']}>
+                    {product.countInStock < 1
+                      ? 'ناموجود'
+                      : product.countInStock < 6
+                      ? `فقط ${product.countInStock} عدد باقی مانده `
+                      : 'موجود در انبار'}
+                  </h5>
+                </Row>
+              </div>
+            </Card>
+          </Link>
+        ))
+      )}
     </div>
   );
 };
